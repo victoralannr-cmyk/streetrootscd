e03dbe364ae140ce07d0f98f5590077c6e85e1df
Victor Alann <victorAlannr@gmail.com>
2025-09-21 07:17:40 +0000

    altere a imagem da segunda sessão pela imagem que vou te mandar https://

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ const barbers = [
 
 const galleryItems = [
   {
-    id: "gallery-9",
+    id: "gallery-10",
     category: "Street Style",
     title: "Fade",
     barber: "Diego Avelino",
@@ -97,7 +97,7 @@ const galleryItems = [
     barber: "Carlos Davi",
   },
    {
-    id: "gallery-10",
+    id: "gallery-9",
     category: "Design",
     title: "Manutenção de Dreads",
     barber: "Carlos Davi",
@@ -227,7 +227,7 @@ export default function Home() {
           id="gallery"
           className="relative w-full py-12 md:py-24 lg:py-32 bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: "url('https://i.postimg.cc/5NwXnztt/youngboy-never-5120x2880-13945.jpg')",
+            backgroundImage: "url('https://i.postimg.cc/sDtmrhFS/87d88046e835fd80d0d9be0a30ed79ca.jpg')",
           }}
         >
           <div className="absolute inset-0 bg-black/70" />
