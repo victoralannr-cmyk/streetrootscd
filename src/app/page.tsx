f79779f6393d38ff4d89515df64f7996a9b30ab9
Victor Alann <victorAlannr@gmail.com>
2025-09-24 01:19:52 +0000

    altere a foto da segunda aba para essa que vou te mandar https://i.posti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -204,7 +204,7 @@ export default function Home() {
           id="gallery"
           className="relative w-full py-12 md:py-24 lg:py-32 bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: "url('https://i.postimg.cc/PxL0DwJk/e4c0048f-97b4-4c26-b72b-03e22bd5d822.jpg')",
+            backgroundImage: "url('https://i.postimg.cc/1RvNXV6z/Captura-de-tela-2025-09-23-221831.png')",
           }}
         >
           <div className="absolute inset-0 bg-black/70" />
@@ -422,5 +422,3 @@ export default function Home() {
     </div>
   );
 }
-
-    
