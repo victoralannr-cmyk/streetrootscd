0bf677c8a53bc8366ac5a74aa31f045d4165f25d
Victor Alann <victorAlannr@gmail.com>
2025-09-24 00:07:23 +0000

    altere a foto da quarta aba pela a foto que vou te mandar https://i.post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -357,7 +357,7 @@ export default function Home() {
         <section
           id="about"
           className="relative w-full py-12 md:py-24 lg:py-32 bg-cover bg-center"
-           style={{ backgroundImage: "url('https://i.postimg.cc/6pbLnGnz/bad927d5544cb67c739adcec8da4fbf0.jpg')" }}
+           style={{ backgroundImage: "url('https://i.postimg.cc/cJKjnWgg/1b8e8fdd0e8607d24fe9b45f9e3fb648.jpg')" }}
         >
           <div className="absolute inset-0 bg-black/70" />
           <div className="container relative px-4 md:px-6">
@@ -535,6 +535,8 @@ export default function Home() {
 
     
 
+
+    
 
     
 
