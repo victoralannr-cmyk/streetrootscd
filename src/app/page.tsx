b519f12b828f5e194dc03a1f337bf33c9ac6b810
Victor Alann <victorAlannr@gmail.com>
2025-09-25 03:00:54 +0000

    altere a foto de fundo da terceira aba por essa que vou te enviar https:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -260,7 +260,7 @@ export default function Home() {
         {/* Pricing Section */}
         <section id="pricing" className="relative w-full py-12 md:py-24 lg:py-32 bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: "url('https://i.postimg.cc/MTPsGrtZ/c7725ce7aca2ffe75e2a0a5d29c0582f.jpg')",
+            backgroundImage: "url('https://i.postimg.cc/JhRc1MbC/f0751715244a1a200f2d618cd67ed6c5.jpg')",
           }}>
            <div className="absolute inset-0 bg-black/70" />
           <div className="container relative px-4 md:px-6">
